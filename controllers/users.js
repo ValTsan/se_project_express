b6d7bdffaad696caae6261fa34a06d6147ea7228
ValerieTsan <valeriejean.tsan@gmail.com>
2025-02-19 12:26:38 -0500

    Fix res status message

--- controllers/users.js
+++ controllers/users.js
@@ -26,6 +26,32 @@ const createUser = (req, res) => {
         .json({ message: "Bad Request" });
     });
 };
+// //GET /:userId
+// const getUser = (req, res) => {
+//   const { userId } = req.params;
+
+//   if (!/^[a-fA-F0-9]{24}$/.test(userId)) {
+//     return res
+//       .status(StatusCodes.BAD_REQUEST)
+//       .json({ message: "Invalid user ID format" });
+//   }
+
+//   User.findById(userId)
+//     .then((user) => {
+//       if (!user) {
+//         return res
+//           .status(StatusCodes.NOT_FOUND)
+//           .json({ message: "User not found" });
+//       }
+//       res.status(StatusCodes.OK).json(user);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res
+//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
+//         .json({ message: "Server error" });
+//     });
+// };
 
 //GET /:userId
 const getUser = (req, res) => {
@@ -35,15 +61,12 @@ const getUser = (req, res) => {
     .then((user) => res.status(StatusCodes.OK).json(user))
     .catch((err) => {
       console.error(err);
-      console.log(err.name);
-      // if (err.name === "") {
-      //   return res
-      //     .status(StatusCodes.BAD_REQUEST)
-      //     .json({ message: "Invalid user ID format" });
-      // }
-      return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: "Server error" });
+      if (err.name === "CastError") {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: "Invalid user ID format" });
+      }
+      return res.status(StatusCodes.NOT_FOUND).json({ message: "Not Found" });
     });
 };
 
