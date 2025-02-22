fe21b4320a5202d7a975a51a34d7a03a4fded425
ValerieTsan <valeriejean.tsan@gmail.com>
2025-02-22 13:50:31 -0500

    Eslint rules added

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
   rules: {
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
     "no-console": ["warn", { allow: ["error"] }],
+    "import/extensions": ["error", "ignorePackages", { js: "never" }],
   },
 };
