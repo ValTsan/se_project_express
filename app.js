7605522d60773ce9354952fef3c4056e9c03e0a6
ValerieTsan <valeriejean.tsan@gmail.com>
2025-04-08 13:11:30 -0400

    Install dotenv

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
